009dcd2af1a87e5dee8103d64722a4a4cae57418
Swapnil Borude <borudeswapnil5@gmail.com>
2026-09-10 08:45:21 +0530

    adding a print to force a trigger

--- main.py
+++ main.py
@@ -229,6 +229,8 @@ async def index():
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 
+
+
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
